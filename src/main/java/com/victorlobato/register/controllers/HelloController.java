94c70046e2f0ef765d8c5b9a200c9db415aae603
victorlobato2000 <victorsilva123190@gmail.com>
2021-11-20 17:36:13 -0300

    Finalizando helloController testes

--- src/main/java/com/victorlobato/register/controllers/HelloController.java
+++ src/main/java/com/victorlobato/register/controllers/HelloController.java
@@ -1,13 +1,33 @@
 package com.victorlobato.register.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello(){
+    @GetMapping("/hello-servlet")
+    public String hello1(HttpServletRequest request){
+        request.setAttribute("nome","Victor");
         return "hello";
     }
+
+    @GetMapping("/hello/model")
+    public String hello2(Model model){
+        model.addAttribute("nome","Victor Silva");
+        return "hello";
+    }
+
+
+    @GetMapping("/hello/model-view")
+    public ModelAndView hello3(){
+        ModelAndView mv = new ModelAndView("hello"); //nome do arquivo html a ser renderizado
+        mv.addObject ("nome","Victor Lobato");
+        return mv;
+    }
+
 }
