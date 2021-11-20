5df45bef9e17136a446bcbc96aa7ba92cf7d0da7
victorlobato2000 <victorsilva123190@gmail.com>
2021-11-20 19:04:54 -0300

    Adicionando ProfessorRepository

---
+++ src/main/java/com/victorlobato/register/repositories/ProfessorRepository.java
@@ -0,0 +1,9 @@
+package com.victorlobato.register.repositories;
+
+import com.victorlobato.register.models.Professor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+}
