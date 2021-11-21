74d28bce9a508b684d064bd860e5680035567c05
victorlobato2000 <victorsilva123190@gmail.com>
2021-11-21 11:27:05 -0300

    Recuperando dados em um erro de validação

--- src/main/java/com/victorlobato/register/controllers/ProfessorController.java
+++ src/main/java/com/victorlobato/register/controllers/ProfessorController.java
@@ -1,27 +1,30 @@
 package com.victorlobato.register.controllers;
 
-import com.victorlobato.register.dto.ProfessorDto;
+import com.victorlobato.register.dto.ProfessorRequest;
 import com.victorlobato.register.enums.StatusProfessor;
 import com.victorlobato.register.models.Professor;
 import com.victorlobato.register.repositories.ProfessorRepository;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @Controller
+@RequestMapping("/professores")
 public class ProfessorController {
 
     @Autowired
     ProfessorRepository professorRepository;
 
-    @GetMapping("/professores")
+    @GetMapping("")
     public ModelAndView index(){
         List<Professor> professores = professorRepository.findAll();
         ModelAndView mv = new ModelAndView("professores/index");
@@ -29,21 +32,26 @@ public class ProfessorController {
         return mv;
     }
 
-    @GetMapping("/professor/new")
-    public ModelAndView nnew(){
+    @GetMapping("/new")
+    public ModelAndView nnew(@ModelAttribute ProfessorRequest professorRequest, Model model){
         ModelAndView mv = new ModelAndView("professores/new");
-        mv.addObject("statusProfessor", StatusProfessor.values());
+        mv.addObject("listaStatusProfessor", StatusProfessor.values());
+        model.addAttribute("professorRequest", professorRequest);
         return mv;
     }
 
-    @PostMapping("/professores")
-    public String create(@Valid ProfessorDto professorDto, BindingResult bindingResult){
+    @PostMapping("")
+    public ModelAndView create(@Valid ProfessorRequest professorRequest, BindingResult bindingResult){
         if(bindingResult.hasErrors()){
-            return "redirect:/professor/new";
+            ModelAndView mv = new ModelAndView("professores/new");
+            mv.addObject("listaStatusProfessor", StatusProfessor.values());
+            return mv;
         }else {
-            Professor professor = professorDto.toProfessor(); //transformando professorDTO em professorModel
+            Professor professor = professorRequest.toProfessor(); //transformando professorDTO em professorModel
             professorRepository.save(professor);
-            return "redirect:/professores";
+            ModelAndView mv = new ModelAndView("redirect:/professores");
+            mv.addObject(professorRequest);
+            return mv ;
         }
     }
 }
--- src/main/java/com/victorlobato/register/dto/ProfessorRequest.java
+++ src/main/java/com/victorlobato/register/dto/ProfessorRequest.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
-public class ProfessorDto {
+public class ProfessorRequest {
 
     @NotBlank
     @NotNull
